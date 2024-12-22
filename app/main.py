12b8f9613bf9e5f6086f580e28d1d007982a1019
Yevf <yevfimiiloboda1276@gmail.com>
2024-12-22 19:37:04 +0200

    fixed structure, please write api in routers dir, then connect in main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import user, auth
+from .routers import user, auth, two
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(two.router)
 
 @app.get("/")
 def root():
